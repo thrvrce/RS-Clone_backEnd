548e79fa1dfc3779f9789a1bcfd47f2ab1b3494d
Victor Avdeev <avdeevvictor95@gmail.com>
2021-01-24 08:16:09 +0300

    add: CORS support

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 import todoRouter from './routes/todos';
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
